d901e6f05a6c6a77ad7795c603bdd129a46fa487
programming7777 <143686660+programming7777@users.noreply.github.com>
2023-08-31 16:05:36 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -1,20 +1,21 @@
 class Test {
-	constructor() {}
-	getInfo() { // 拡張機能の各種情報
-		return {
-			id: 'test',
-			name: 'Test', // 拡張機能の名前
-			blocks: [ // 各ブロックの定義
-				{
-					opcode: 'hello',
-					blockType: Scratch.BlockType.COMMAND,
-					text: 'hello'
-				}
-			]
-		}
-	}
-	hello() {
-		console.log('hello'); // console log に hello と出力
-	}
+  constructor() {}
+  getInfo() { // 拡張機能の各種情報
+    return {
+      id: 'test',
+      name: 'Test', // 拡張機能の名前
+      blocks: [ // 各ブロックの定義
+        {
+          opcode: 'hello', // このブロックが実行されると、helloという関数が呼ばれる
+          blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
+          text: 'hello' // ブロックに表示されるテキスト
+        }
+      ]
+    }
+  }
+  hello() {
+    console.log('hello'); // console log に hello と出力
+  }
 }
+
 Scratch.extensions.register(new Test());
